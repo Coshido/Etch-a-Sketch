858d9c5b59f6663b8fa6592c0ba62a35a3339d86
Coshido <coshido@gmail.com>
2022-12-06 17:03:21 +0100

    add selector function and style to setting buttons

--- javascript.js
+++ javascript.js
@@ -21,4 +21,31 @@ function createGrid(){
     }
 }
 
+function clearButtonColors(){
+    const switchButtonList = [...document.querySelectorAll('.switchButton')];
+    for (let ele in switchButtonList){
+        switchButtonList[ele].setAttribute('style','color: #212529; background-color: #F8F9FA;');;
+    }
+  //  .setAttribute('style','color: #212529; background-color: #F8F9FA;');
+    
+}
+function modeSelector(string){
+    clearButtonColors();
+    switch (string){
+        case 'color':
+            document.querySelector('#color').setAttribute('style','color: #F8F9FA; background-color: #343A40;');
+            colorMode();
+            break;
+        case 'rainbow':
+            document.querySelector('#rainbow').setAttribute('style','color: #F8F9FA; background-color: #343A40;');
+            rainbowMode();
+            break;
+        case 'eraser':
+            document.querySelector('#eraser').setAttribute('style','color: #F8F9FA; background-color: #343A40;');
+            eraserMode();
+            break;
+    }
+    
+}
+
 createGrid();
