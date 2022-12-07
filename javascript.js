0d9dfdfa33f26a460dc07b635800f3023bc41cd3
Coshido <coshido@gmail.com>
2022-12-07 16:20:43 +0100

    add event listener to change grid color

--- javascript.js
+++ javascript.js
@@ -1,18 +1,34 @@
 const gridContainer = document.querySelector('#sketch');
+let mode = "black";
+let mouseDown = false;
+document.body.onmousedown = function(){
+    mouseDown = true;
+}
+document.body.onmouseup = function() {
+    mouseDown = false;
+}
 
 function clearGrid(){
     gridContainer.innerHTML = "";
+}
+function changeColor(e){
+    if(mouseDown){
+        e.target.style.backgroundColor = "black";
+    }
 }
 
 function appendDiv(){
         const content = document.createElement('div');
         content.classList.add('grid-item');
+        content.addEventListener('mousedown', changeColor);
+        content.addEventListener('mouseover', changeColor);
         gridContainer.prepend(content);    
 }
 
 function createGrid(){
     clearGrid();
     let value = document.querySelector('#slider').value;
+    document.querySelector('#slider-text').textContent = `Grid size: ${value} x ${value}`;
     gridContainer.style.setProperty('--grid-cols', value);
     gridContainer.style.setProperty('--grid-rows', value);
     let numberOfChilds= value * value;
@@ -34,15 +50,15 @@ function modeSelector(string){
     switch (string){
         case 'color':
             document.querySelector('#color').setAttribute('style','color: #F8F9FA; background-color: #343A40;');
-            colorMode();
+            mode = "color";
             break;
         case 'rainbow':
             document.querySelector('#rainbow').setAttribute('style','color: #F8F9FA; background-color: #343A40;');
-            rainbowMode();
+            mode = "rainbow";
             break;
         case 'eraser':
             document.querySelector('#eraser').setAttribute('style','color: #F8F9FA; background-color: #343A40;');
-            eraserMode();
+            mode = "eraser";
             break;
     }
     
